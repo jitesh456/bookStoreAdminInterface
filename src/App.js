f68887f25d68b86f41216be0859649cfb61046c6
rajnish432 <rajnish.kahar1996@gmail.com>
2020-05-02 20:16:21 +0530

    ADDED: Code For Admin Add Books UI

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import AddBook from './component/AddBook';
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <h3>Book Store Admin Interface </h3>
       </header>
+      <div className="admin-form">
+        <AddBook/>
+      </div>
     </div>
   );
 }
